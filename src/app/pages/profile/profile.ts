e86af935ce2e6fcb762cadd7a0f1c77b9af38d60
EdgarsLv <edgars0707@gmail.com>
2025-07-22 00:34:18 +0300

    installed primeng and tailwind

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { StatsWidget } from '../../components/statswidget';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ButtonModule, StatsWidget],
   templateUrl: './profile.html',
-  styleUrl: './profile.scss'
+  styleUrl: './profile.scss',
 })
-export class Profile {
-
-}
+export class Profile {}
