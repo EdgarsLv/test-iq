c5dca55cc8bfc540350828bea5664ebedc0e347c
EdgarsLv <edgars0707@gmail.com>
2025-07-23 23:22:20 +0300

    home restyled

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
-export class Home {
-
-}
+export class Home {}
